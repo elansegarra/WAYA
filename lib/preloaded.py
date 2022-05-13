bc3a303290a8738d191ae0ed8ea4240a2b643175
Elan Segarra <elanster@gmail.com>
2022-05-12 20:43:34 -0400

    Internalized epub file paths

--- lib/preloaded.py
+++ lib/preloaded.py
@@ -3,35 +3,37 @@ from ebooklib import epub
 from bs4 import BeautifulSoup
 
 # Defining the preloaded series and associated files
-preloaded_dicts = {"Wheel of Time":{ 
+folder_wot = "C:\\Users\\Phoenix\\Documents\\Literature\\Fiction\\Robert Jordan\\"
+preloaded_dicts = {
+    "Wheel of Time":{ 
                         "books":[{"title": "", "book_num": 1,
-                                "filename": "Jordan, Robert - 01 - The Eye of the World.epub"},
+                "filename": folder_wot+"Jordan, Robert - 01 - The Eye of the World.epub"},
                             {"title": "", "book_num": 2,
-                                    "filename": "Jordan, Robert - 02 - The Great Hunt.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 02 - The Great Hunt.epub"},
                             {"title": "", "book_num": 3,
-                                    "filename": "Jordan, Robert - 03 - The Dragon Reborn.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 03 - The Dragon Reborn.epub"},
                             {"title": "", "book_num": 4,
-                                    "filename": "Jordan, Robert - 04 - The Shadow Rising.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 04 - The Shadow Rising.epub"},
                             {"title": "", "book_num": 5,
-                                    "filename": "Jordan, Robert - 05 - The Fires of Heaven.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 05 - The Fires of Heaven.epub"},
                             {"title": "", "book_num": 6,
-                                    "filename": "Jordan, Robert - 06 - Lord of Chaos.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 06 - Lord of Chaos.epub"},
                             {"title": "", "book_num": 7,
-                                    "filename": "Jordan, Robert - 07 - A Crown of Swords.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 07 - A Crown of Swords.epub"},
                             {"title": "", "book_num": 8,
-                                    "filename": "Jordan, Robert - 08 - The Path of Daggers.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 08 - The Path of Daggers.epub"},
                             {"title": "", "book_num": 9,
-                                    "filename": "Jordan, Robert - 09 - Winter's Heart.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 09 - Winter's Heart.epub"},
                             {"title": "", "book_num": 10,
-                                    "filename": "Jordan, Robert - 10 - Crossroads of Twilight.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 10 - Crossroads of Twilight.epub"},
                             {"title": "", "book_num": 11,
-                                    "filename": "Jordan, Robert - 11 - Knife of Dreams.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 11 - Knife of Dreams.epub"},
                             {"title": "", "book_num": 12,
-                                    "filename": "Jordan, Robert - 12 - The Gathering Storm.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 12 - The Gathering Storm.epub"},
                             {"title": "", "book_num": 13,
-                                    "filename": "Jordan, Robert - 13 - Towers of Midnight.epub"},
+                    "filename": folder_wot+"Jordan, Robert - 13 - Towers of Midnight.epub"},
                             {"title": "", "book_num": 14,
-                                    "filename": "Jordan, Robert - 14 - A Memory of Light.epub"}
+                    "filename": folder_wot+"Jordan, Robert - 14 - A Memory of Light.epub"}
                             ]},
                     "Lord of the Rings": {
                         "books":[{"title": "The Fellowship of the Ring",
@@ -152,23 +154,21 @@ wot_bs_tags = {1:{'element':"p", 'class': ["h4","h3 sgc-2", "tx15 sgc-2"]},
 
 # Extract chapters from each books of WoT
 series_index = "Wheel of Time"
-folder_wot = "C:\\Users\\Phoenix\\Documents\\Literature\\Fiction\\Robert Jordan\\"
 for book_dict in preloaded_dicts[series_index]["books"]:
     # Set some of the basic book attributes
-    filename = book_dict["filename"]
-    file_full_path = folder_wot+filename
-    book = epub.read_epub(file_full_path)
+    file_path = book_dict["filename"]
+    book = epub.read_epub(file_path)
     book_dict["title"] = book.get_metadata("DC", "title")[0][0]
-    book_dict["file_type"] = filename[filename.find(".")+1:]
+    book_dict["file_type"] = file_path[file_path.find(".")+1:]
     book_dict["chapters"] = [{'name':"TBD", 'text':"",'bs_sec':0}]
 
 for book_dict in preloaded_dicts[series_index]["books"][0:6]:#0,1,2,3,4]: #[0,1,2]:
     # Grab the filename and some metatdata
-    filename = book_dict["filename"]
+    file_path = book_dict["filename"]
     book_num = book_dict['book_num']
-    file_full_path = folder_wot+filename
+    # file_full_path = folder_wot+filename
     # Extract the chapters
-    ch_data = extract_chapters(file_full_path, secs = wot_sec_dicts[book_num], 
+    ch_data = extract_chapters(file_path, secs = wot_sec_dicts[book_num], 
                                 method_label = wot_mlabels[book_num],
                                 title_bs_tags = wot_bs_tags[book_num])
     # print(ch_data)
@@ -178,7 +178,7 @@ for book_dict in preloaded_dicts[series_index]["books"][0:6]:#0,1,2,3,4]: #[0,1,
 
 book_index = 4
 
-file_loader_obj1 = folder_wot+preloaded_dicts[series_index]["books"][book_index]["filename"]
+file_loader_obj1 = preloaded_dicts[series_index]["books"][book_index-1]["filename"]
 book = epub.read_epub(file_loader_obj1)
 items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
 
