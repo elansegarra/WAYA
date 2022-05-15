6e5847c17c5f7b9504ca9ad435ec3b39ffb935e2
Elan Segarra <elanster@gmail.com>
2022-05-15 01:35:05 -0400

    Small tweak to sec definition thresh and cleaned code a bit

--- lib/epub_parser.py
+++ lib/epub_parser.py
@@ -31,6 +31,7 @@ def extract_chapters(file_loader_obj, secs = None, method_label = None,
 
 def extract_ch_data(html, method_label = None, title_bs_tags = None):
     # Takes HTML and returns the title
+
     soup = BeautifulSoup(html, 'html.parser')
     # text = soup.find_all(text=True)
     # text = soup.get_text()
@@ -42,7 +43,7 @@ def extract_ch_data(html, method_label = None, title_bs_tags = None):
         # Extract all the body text (everything after last element of ch title)
         all_text = soup.get_text() 
         ch_text = all_text[all_text.find(ch_title[-1])+len(ch_title[-1]):].strip()
-        ch_text = ch_text[0:25]+'  ...  '+ch_text[-25:]
+        # ch_text = ch_text[0:25]+'  ...  '+ch_text[-25:]
         # Cleaning titles (must do after getting rest of text, or it won't match title found in text)
         ch_title = [' '.join([wd.strip() for wd in text.split()]) for text in ch_title]
         # Assemble chapter info dict
@@ -80,7 +81,7 @@ def get_relevant_secs(epub_file):
     # Open the epub file
     book = epub.read_epub(epub_file)
     items = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-    
+
     # Get the number of characters from the actual text of each section
     sec_lens = []
     for i in range(len(items)):
@@ -89,7 +90,7 @@ def get_relevant_secs(epub_file):
         sec_lens.append(len(soup.get_text()))
     
     # Section needs at least 20% of median length to be relevant
-    char_len_thresh = median(sec_lens)/5 
-    relevant_secs = [i for i in range(len(sec_lens)) if sec_lens[i] >= char_len_thresh]
+    char_len_thresh = median(sec_lens)/10
+    relevant_secs = [i for i in range(1,len(sec_lens)) if sec_lens[i] >= char_len_thresh]
 
     return relevant_secs
